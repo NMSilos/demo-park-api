1d30c465bd3bc96a29e45a5155012f6b8e39482e
Nicolas <nicolas.marsil02@gmail.com>
2024-04-25 14:42:58 -0300

    feat: metodo create para Usuario

--- src/main/java/com/nmsilos/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/nmsilos/demoparkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.nmsilos.demoparkapi.service;
 
+import com.nmsilos.demoparkapi.entity.Usuario;
 import com.nmsilos.demoparkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,10 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 
 }
--- src/main/java/com/nmsilos/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/nmsilos/demoparkapi/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.nmsilos.demoparkapi.web.controller;
 
+import com.nmsilos.demoparkapi.entity.Usuario;
 import com.nmsilos.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
 }
