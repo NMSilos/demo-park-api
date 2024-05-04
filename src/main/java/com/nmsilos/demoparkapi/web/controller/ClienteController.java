e2339fff0a1479638c8f70d027a0d92de86e9252
Nicolas <nicolas.marsil02@gmail.com>
2024-05-04 16:29:15 -0300

    feat: teste ponto a ponto buscarPorId (Status 200)

--- src/main/java/com/nmsilos/demoparkapi/web/controller/ClienteController.java
+++ src/main/java/com/nmsilos/demoparkapi/web/controller/ClienteController.java
@@ -50,7 +50,18 @@ public class ClienteController {
         return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
     }
 
+    @Operation(summary = "Localizar um cliente", description = "Recurso para localizar um cliente pelo ID. " +
+            "Requisição exige uso de um bearer tooken. Acesso restrito a Roles 'ADMIN'",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso localizadoo com sucesso",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Cliente não encontrado",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ClienteResponseDto> getbyId(@PathVariable Long id) {
         Cliente cliente = clienteService.buscarPorId(id);
         return ResponseEntity.ok(ClienteMapper.toDto(cliente));
