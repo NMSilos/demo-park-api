4c66cf2ef89cc6389804705dad6f7c2c155b98f0
Nicolas <nicolas.marsil02@gmail.com>
2024-05-05 15:33:43 -0300

    feat: teste recurso criar vaga (Status 200, 409 e 422)

--- src/main/java/com/nmsilos/demoparkapi/web/controller/VagaController.java
+++ src/main/java/com/nmsilos/demoparkapi/web/controller/VagaController.java
@@ -2,11 +2,20 @@ package com.nmsilos.demoparkapi.web.controller;
 
 import com.nmsilos.demoparkapi.entity.Vaga;
 import com.nmsilos.demoparkapi.service.VagaService;
+import com.nmsilos.demoparkapi.web.dto.UsuarioResponseDto;
 import com.nmsilos.demoparkapi.web.dto.VagaCreateDto;
 import com.nmsilos.demoparkapi.web.dto.VagaResponseDto;
 import com.nmsilos.demoparkapi.web.dto.mapper.VagaMapper;
+import com.nmsilos.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +30,16 @@ public class VagaController {
 
     private final VagaService vagaService;
 
+    @Operation(summary = "Criar uma nova vaga", description = "Requisição exige um Bearer Token. Acesso restrito a Role=ADMIN",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = VagaResponseDto.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
---
+++ src/test/java/com/nmsilos/demoparkapi/VagaIT.java
@@ -0,0 +1,79 @@
+package com.nmsilos.demoparkapi;
+
+import com.nmsilos.demoparkapi.web.dto.VagaCreateDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/vagas/vagas-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/vagas/vagas-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class VagaIT {
+
+    @Autowired
+    private WebTestClient testClient;
+
+    @Test
+    public void criarVaga_ComDadosValidos_RetornarLocationStatus201() {
+        testClient
+                .post()
+                .uri("api/v1/vagas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(new VagaCreateDto("A-05", "LIVRE"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists(HttpHeaders.LOCATION);
+    }
+
+    @Test
+    public void criarVaga_ComCodigoJaExistente_RetornarErrorMessageComStatus409() {
+        testClient
+                .post()
+                .uri("api/v1/vagas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(new VagaCreateDto("A-01", "LIVRE"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody()
+                .jsonPath("status").isEqualTo("409")
+                .jsonPath("method").isEqualTo("POST")
+                .jsonPath("path").isEqualTo("/api/v1/vagas");
+    }
+
+    @Test
+    public void criarVaga_ComDadosInvalidos_RetornarErrorMessageComStatus422() {
+        testClient
+                .post()
+                .uri("api/v1/vagas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(new VagaCreateDto("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody()
+                .jsonPath("status").isEqualTo("422")
+                .jsonPath("method").isEqualTo("POST")
+                .jsonPath("path").isEqualTo("/api/v1/vagas");
+
+        testClient
+                .post()
+                .uri("api/v1/vagas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .bodyValue(new VagaCreateDto("A-501", "DESOCUPADA"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody()
+                .jsonPath("status").isEqualTo("422")
+                .jsonPath("method").isEqualTo("POST")
+                .jsonPath("path").isEqualTo("/api/v1/vagas");
+    }
+
+
+}
