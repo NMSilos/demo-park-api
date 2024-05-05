558be8f9ef5caee079c0c7410db1a7e4c6284d4e
Nicolas <nicolas.marsil02@gmail.com>
2024-05-05 11:24:35 -0300

    feat: classe Vaga e interface VagaRepository

---
+++ src/main/java/com/nmsilos/demoparkapi/repository/VagaRepository.java
@@ -0,0 +1,7 @@
+package com.nmsilos.demoparkapi.repository;
+
+import com.nmsilos.demoparkapi.entity.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+}
