6c69b44f79b84374e0332586cf66be08b72a7d98
Nicolas <nicolas.marsil02@gmail.com>
2024-05-08 19:04:45 -0300

    feat: classe ClienteVagaMapper

---
+++ src/main/java/com/nmsilos/demoparkapi/web/dto/mapper/ClienteVagaMapper.java
@@ -0,0 +1,21 @@
+package com.nmsilos.demoparkapi.web.dto.mapper;
+
+import com.nmsilos.demoparkapi.entity.ClienteVaga;
+import com.nmsilos.demoparkapi.web.dto.EstacionamentoClienteDto;
+import com.nmsilos.demoparkapi.web.dto.EstacionamentoResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClienteVagaMapper {
+
+    public static ClienteVaga toClienteVaga(EstacionamentoClienteDto dto) {
+        return new ModelMapper().map(dto, ClienteVaga.class);
+    }
+
+    public static EstacionamentoResponseDto toDto(ClienteVaga clienteVaga) {
+        return new ModelMapper().map(clienteVaga, EstacionamentoResponseDto.class);
+    }
+
+}
